bb842a7702280b7e01bf90d68534eedb7066be0c
Hossein Baghayi <hossein.baghayi@gmail.com>
2018-09-15 11:49:26 +0430

    find an event by its id

--- popup.js
+++ popup.js
@@ -110,6 +110,20 @@ function run (jwt) {
         }));
     }
 
+    function fetchEventDataById (eventId, onSuccessCallback) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            if (xhr.readyState == 4) {
+                const event = JSON.parse(xhr.responseText).data;
+                onSuccessCallback(event);
+            }
+        };
+
+        xhr.open("GET", "https://api.evand.com/events/id_" + eventId, true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.send();
+    }
+
     let loginButton = document.getElementById('login');
     loginButton.addEventListener('click', function(){
         let email = document.getElementById('login-email');
@@ -117,6 +131,31 @@ function run (jwt) {
 
         logUserIn(email.value, password.value);
     });
+
+    (function () {
+        let eventFinderById  = document.querySelector("p#event-finder-by-id input");
+        let foundEventLink  = document.querySelector("p#event-finder-by-id a");
+        eventFinderById.addEventListener('change', function(e){
+            let bulletin  = document.querySelector("p#event-finder-by-id span");
+            foundEventLink.style = "display: none";
+            bulletin.innerHTML = "Searching ...";
+            bulletin.style = "display: inline;";
+
+            const eventId = e.target.value;
+            const event = fetchEventDataById(eventId, function(event){
+                if(event == undefined) {
+                    bulletin.innerHTML = "Not Found!";
+                    return;
+                }
+
+                bulletin.style = "display: none";
+                foundEventLink.style = "display: inline";
+                foundEventLink.innerHTML = event.name;
+                foundEventLink.href = "https://evand.com/events/" + event.slug;
+            });
+        });
+    })();
+
 }
 
 
