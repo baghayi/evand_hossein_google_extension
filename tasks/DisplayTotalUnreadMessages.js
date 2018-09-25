bf4b2447fb7f52e62ed836a6d04b34d4e3de6e7f
Hossein Baghayi <hossein.baghayi@gmail.com>
2018-09-25 17:49:06 +0330

    total unread messages

---
+++ tasks/DisplayTotalUnreadMessages.js
@@ -0,0 +1,42 @@
+'use strict';
+
+function fetchTotalUnreadUserMessages (jwt) {
+    return fetch('https://api.evand.com/users/me?fields=messages', {
+        headers: {
+            "Authorization": jwt,
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        }
+        })
+    .then(function(response){
+        if(response.status >= 200 && response.status < 300) {
+            return Promise.resolve(response);
+        }else {
+            return Promise.reject(new Error(response.statusText));
+        }
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(user){
+        return user.data.messages.unread.count;
+    });
+}
+
+setInterval(function(){
+    chrome.storage.sync.get(['jwt'], function(result){
+        const jwt = result.jwt;
+        if(jwt) {
+            fetchTotalUnreadUserMessages(jwt)
+                .then(function(totalUnreadMessages){
+                    chrome.browserAction.setBadgeText({
+                        text: totalUnreadMessages.toString()
+                    });
+                });
+        }
+    });
+}, 5000);
+
+chrome.tabs.onActivated.addListener(function(activeInfo){
+
+});
