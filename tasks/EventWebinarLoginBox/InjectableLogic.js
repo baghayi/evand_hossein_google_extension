b7b31bac9fd713a97f3a8959c2d7b076173df9af
Hossein Baghayi <hossein.baghayi@gmail.com>
2018-10-23 17:28:11 +0330

    working on listing ordered tickets

--- tasks/EventWebinarLoginBox/InjectableLogic.js
+++ tasks/EventWebinarLoginBox/InjectableLogic.js
@@ -1,50 +1,68 @@
-(function(event){
+const ticket = 'xyz123';
+function getTemplate(orderedTickets) {
     const template = `
-              <style>
-              input#ticket-identifier {
-                  padding: 5px;
-                  margin-left: 5px;
-                  width: 80px;
-              }
-              input#goto-verification-step {
-                  padding: 5px;
-              }
-              p#webinar-login-as-organizer {
-                  direction: rtl; text-align: center;
-              }
-              p#webinar-login-as-organizer > a {
-                  font-size: .7em;
-                  font-weight: bold;
-                  cursor: pointer;
-              }
-              div.webinar  h3 {
-                  text-align: center;
-              }
-              div#inputs {
-                  text-align: center;
-              }
-              </style>
-              <div class="webinar">
-              <h3>ورودیه وبینار</h3>
-              <div id="inputs">
-              <input list="event-ordered-tickets" type="text" placeholder="کد بلیت" id="ticket-identifier">
-              <datalist id="event-ordered-tickets">
-              <option value="xyz">
-              </datalist>
-              <input type="button" value="ورود" id="goto-verification-step">
-              </div>
+        <style>
+        input#ticket-identifier {
+            padding: 5px;
+                     margin-left: 5px;
+                     width: 80px;
+        }
+    input#goto-verification-step {
+        padding: 5px;
+    }
+    p#webinar-login-as-organizer {
+        direction: rtl; text-align: center;
+    }
+    p#webinar-login-as-organizer > a {
+        font-size: .7em;
+        font-weight: bold;
+        cursor: pointer;
+    }
+    div.webinar  h3 {
+        text-align: center;
+    }
+    div#inputs {
+        text-align: center;
+    }
+    </style>
+        <div class="webinar">
+        <h3>ورودیه وبینار</h3>
+        <div id="inputs">
+        <input list="event-ordered-tickets" type="text" placeholder="کد بلیت" id="ticket-identifier">
+        <datalist id="event-ordered-tickets">
+        ${orderedTickets}
+    <option value="${ticket}">
+        </datalist>
+        <input type="button" value="ورود" id="goto-verification-step">
+        </div>
 
-              <p id="webinar-login-as-organizer"><a>ورود به عنوان برگزار کننده</a></p>
-              </div>
-              `;
+        <p id="webinar-login-as-organizer"><a>ورود به عنوان برگزار کننده</a></p>
+        </div>
+        `;
+
+    return template;
+}
+
+function getJwt() {
+    const cookies = document.cookie.split('; ').map(x => x.split('=');
+    let jwt = null;
+    cookies.forEach(x => x[0] == 'jwt' ? jwt = decodeURIComponent(x[1]) : '')
+    return jwt;
+}
+
+function orderedTickets(eventId) {
+    fetch()
+}
+
+(function(event){
 
     const EVAND_API = 'https://api.evand.com';
 
     function displayVerificationItems(shadowRoot, verificationUri, ticket) {
         const html = `
-                      <input type="text" placeholder="کد تاییدیه" id="verification-code">
-                      <input type="button" value="ورود به وبینار" id="jump-into-webinar">
-                      `;
+            <input type="text" placeholder="کد تاییدیه" id="verification-code">
+            <input type="button" value="ورود به وبینار" id="jump-into-webinar">
+            `;
 
         const inputs = shadowRoot.getElementById('inputs');
         inputs.innerHTML = html;
@@ -124,7 +142,7 @@
     let webinarBox = document.querySelector('div.event-location-webinar').parentNode.parentNode;
     if(webinarBox) {
         let shadowRoot = webinarBox.attachShadow({mode: "open"});
-        shadowRoot.innerHTML = template;
+        shadowRoot.innerHTML = template(orderedTickets(14685));
 
         shadowRoot.querySelector('p#webinar-login-as-organizer > a').addEventListener('click', function(e){
             console.log('login as organizer');
@@ -135,14 +153,12 @@
             console.log('login as attendee');
 
             // if jwt is available, send a request with JWT and ticket, if faced any access issues then fallback to identifier token
-            const cookies = document.cookie.split('; ').map(x => x.split('='))
-            let jwt = null;
-            cookies.forEach(x => x[0] == 'jwt' ? jwt = decodeURIComponent(x[1]) : '')
-            if(jwt != null) {
-                requestWebinarLoginURL(jwt, null, identifier.value, shadowRoot);
-            }else {
-                requestIdentifierToken(shadowRoot, identifier.value);
-            }
+            const jwt = getJwt();
+                if(jwt != null) {
+                    requestWebinarLoginURL(jwt, null, identifier.value, shadowRoot);
+                }else {
+                    requestIdentifierToken(shadowRoot, identifier.value);
+                }
         });
     }
 
