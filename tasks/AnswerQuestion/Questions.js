b0020ad0ced324e406a4fc66acef9a45059a2a80
Hossein Baghayi <hossein.baghayi@gmail.com>
2019-01-02 11:33:42 +0330

    display wheather or not an event is private

--- tasks/AnswerQuestion/Questions.js
+++ tasks/AnswerQuestion/Questions.js
@@ -8,7 +8,8 @@ export class Event extends AnswerQuestion {
             {label: "Has Active Webinar", findAnswer: (object) => object._links.webinar != null, order: 2},
             //{label: "Has Connect-App", findAnswer: (object) => object.connectApp != undefined },
             {label: "Is Using Showtime", findAnswer: (object) => object.is_using_showtime && true, order: 3},
-            {label: "Cancelled?", findAnswer: (object) => object.cancelled, order: 4}
+            {label: "Cancelled?", findAnswer: (object) => object.cancelled, order: 4},
+            {label: "Private?", findAnswer: (object) => object.private == "yes", order: 5}
         ];
     }
 }
@@ -18,8 +19,8 @@ export class EventStatistics extends AnswerQuestion {
     constructor() {
         super();
         this.questions = [
-            {label: "Total Attendees", findAnswer: (data) => data.attendees.all.count + " of " + data.tickets.all.count, order: 5},
-            {label: "Total Guest Attendees", findAnswer: (data) => data.attendees.guest.count, order: 6}
+            {label: "Total Attendees", findAnswer: (data) => data.attendees.all.count + " of " + data.tickets.all.count, order: 6},
+            {label: "Total Guest Attendees", findAnswer: (data) => data.attendees.guest.count, order: 7}
         ];
 
     }
