fb1c62060f9a0b5f9f6c55cf41da45821a53dfeb
Hossein Baghayi <hossein.baghayi@gmail.com>
2019-03-18 00:35:43 +0330

    move component registration into app config

--- module/whoami/component/main.js
+++ module/whoami/component/main.js
@@ -1,13 +1,29 @@
+import {Config as UserConfig} from './user.js';
+import {Config as WhoamiStatusConfig} from './whoami-status.js';
+import { TokenStorage } from './../TokenStorage.js';
+import { Cookie } from './../Cookie.js';
 
 export class Config
 {
+    constructor(eventBus) {
+        this.eventBus = eventBus;
+    }
+
     getConfig(jwt)
     {
+        const eventBus = this.eventBus;
+        const tokenStorage = new TokenStorage;
+        const cookies = new Cookie();
+
         return {
             el: "#app",
             data: {
                 someone: "",
                 users: []
+            },
+            components: {
+                'whoami-status': (new WhoamiStatusConfig(eventBus, tokenStorage, cookies)).getConfig(),
+                'user': (new UserConfig(eventBus, tokenStorage, cookies)).getConfig(jwt)
             },
             methods: {
                 SearchForSomeone: async function() {
--- module/whoami/main.js
+++ module/whoami/main.js
@@ -1,21 +1,12 @@
 import { Main } from '../../services/Main.js';
-import { TokenStorage } from './TokenStorage.js';
-import { Cookie } from './Cookie.js';
-import {Config as UserConfig} from './component/user.js';
-import {Config as WhoamiStatusConfig} from './component/whoami-status.js';
 import {Config as MainConfig} from './component/main.js';
 
-const tokenStorage = new TokenStorage;
-const cookies = new Cookie();
 let eventBus = new Vue({});
 
 const main = new Main();
 main.run(function(jwt, tabUrl){
-
     Vue.use(AsyncComputed);
-    Vue.component('whoami-status', (new WhoamiStatusConfig(eventBus, tokenStorage, cookies)).getConfig());
-    Vue.component('user', (new UserConfig(eventBus, tokenStorage, cookies)).getConfig(jwt));
-    var app = new Vue((new MainConfig).getConfig(jwt));
+    var app = new Vue((new MainConfig(eventBus)).getConfig(jwt));
 
     eventBus.$on('EvandPageRefreshRequired', refreshEvandPageListener);
 
